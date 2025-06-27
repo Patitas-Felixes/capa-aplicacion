f605fce024978fe8499dad7033cee805439ebf00
Seroluce <141604179+Seroluce@users.noreply.github.com>
2025-06-27 03:21:57 -0500

    feat(models): agregar modelo para Pedido
    Se añadió el modelo `Pedido` utilizando Mongoose, que incluye los esquemas `pedido` y `detallePedido` con validaciones para los campos `cliente_id`, `fecha_pedido`, `total`, `estado`, `direccion_envio` y una lista de `detalles` que hace referencia a productos. Además, se configuraron `timestamps` y se eliminó el uso de `versionKey`.

---
+++ models/pedido.model.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const detallePedidoSchema = new mongoose.Schema(
+    {
+        producto_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "productos",
+            required: true
+        },
+        cantidad: {
+            type: Number,
+            required: true,
+            min: 1
+        },
+        precio_unitario: {
+            type: Number,
+            required: true,
+            min: 0,
+        },
+    }
+);
+
+const pedidoSchema = new mongoose.Schema(
+    {
+        cliente_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "usuarios",
+            required: true
+        },
+        fecha_pedido: {
+            type: Date,
+            default: Date.now
+        },
+        total: {
+            type: Number,
+            required: true,
+            min: 0
+        },
+        estado: {
+            type: String,
+            enum: ["Pendiente", "Enviado", "Entregado", "Cancelado"],
+            required: true,
+            default: "Pendiente"
+        },
+        direccion_envio: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        detalles: {
+            type: [detallePedidoSchema],
+            required: true
+        },
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+);
+
+const Pedido = mongoose.model("Pedido", pedidoSchema);
+module.exports = Pedido;
