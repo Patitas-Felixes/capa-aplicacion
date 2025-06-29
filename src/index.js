de3861a25cad7671f66ff140665f194e832605c4
AmirbarrabajaLeon <amircasan@gmail.com>
2025-06-29 13:29:45 -0500

    chore(index.js): imported models

--- src/index.js
+++ src/index.js
@@ -10,7 +10,13 @@ app.use(express.json());   //Para leer los formatos json del body
 
 app.use(require("./routes/index.routes.js"));
 
-app.use(require("./userModel")) //Usa los modelos de BD
+//importamos los modelos de BD
+app.use(require("./models/categoria.model.js"))
+app.use(require("./models/comentario.model.js"))
+app.use(require("./models/marca.model.js"))
+app.use(require("./models/pedido.model.js"))
+app.use(require("./models/producto.model.js"))
+app.use(require("./models/usuario.model.js"));
 
 // Puerto del Servicio Web
 app.listen(3000);
